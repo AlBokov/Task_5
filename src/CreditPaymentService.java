86734ce36b671181640834173fbe2f333da9e50e
AlBokov <95411491+AlBokov@users.noreply.github.com>
2024-03-14 00:13:52 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
     public double calculate(int loanAmount, int termYears, double percent) {
-        double s = 12 * termYears;  // Срок выплаты кредита в месяцая
-        double m = percent / (100 * 12); // Месячная процентная ставка по кредиту
-        double k = (m * Math.pow((1 + m), s) / (Math.pow((1 + m), s) - 1)); // Коэффициент аннуитета
-        double res = loanAmount * k; // Поиск платежа за месяц
+        double paymentPeriodMonths = 12 * termYears;  // Срок выплаты кредита в месяцая
+        double monthlyInterestRate = percent / (100 * 12); // Месячная процентная ставка по кредиту
+        double annuityRate = (monthlyInterestRate * Math.pow((1 + monthlyInterestRate), paymentPeriodMonths) / (Math.pow((1 + monthlyInterestRate), paymentPeriodMonths) - 1)); // Коэффициент аннуитета
+        double res = loanAmount * annuityRate; // Поиск платежа за месяц
         return res;
     }
 }
